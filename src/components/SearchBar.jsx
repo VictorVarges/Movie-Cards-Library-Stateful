a7aff637b1be7da35311b16c0c7b9d80d4bb19b3
Victor Varges <victorryandias@gmail.com>
2021-09-07 16:33:14 -0300

    definindo props, no requisito 1; No  2, 3 criando atributo no form e label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
-// implement SearchBar component here
+import react from 'react';
+import movies from '/'
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onBookmarkedChange,
+      bookmarkedOnly,
+      onSearchTextChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label"
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
+// this.state = {
+//   searchText: '',
+//   onSearchTextChange: () => (undefined),
+//   bookmarkedOnly: false,
+//   onBookmarkedChange: () => (undefined),
+//   selectedGenre: '',
+//   onSelectedGenreChange: () => (undefined)
+// }
