80572c7bd0d1c25ede546480ad9cb8ba4b0c39cb
Victor Varges <victorryandias@gmail.com>
2021-09-07 17:46:27 -0300

    requisito 5, criando selected e suas respectivas tags

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,15 +13,40 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="searchText" data-testid="text-input-label" >
+        <label htmlFor="searchText" data-testid="text-input-label">
+          Inclui o texto:
           <input
             type="text"
             name="searcheText"
             data-testid="text-input"
-            value={ searchText }
+            value={searchText}
             // bySilas
-            onChange={ onSearchTextChange }
+            onChange={onSearchTextChange}
           />
+        </label>
+        <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="bookmarkedOnly"
+            data-testid="checkbox-input"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+        </label>
+        <label htmlFor="selectedGenre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name="selectedGenre"
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
